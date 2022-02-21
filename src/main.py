74cb22e250a82e52fb8a58478c0236b047894767
Vladislav Illarionov <vladislavillarionov32@gmail.com>
2022-02-21 05:28:30 +0300

    change in main.py for graph8c.txt partial solution

--- src/main.py
+++ src/main.py
@@ -3,12 +3,19 @@ from scripts.load_graphdata import load_graphs, GDataFname
 from scripts.save_graphdata import save_results
 
 
-def solve_whole_dataset(graphdata_fname: str, p_max: int = 1) -> None:
+def solve_dataset(
+            graphdata_fname: str,
+            p_max: int = 1,
+            part_range: tuple[int] = None
+        ) -> None:
     G_data = load_graphs(graphdata_fname)
-    res_df = get_results(G_data, p_max)
-    save_results(res_df, graphdata_fname.replace('.txt', '.csv'))
+    res_df = get_results(G_data, p_max, part_range=part_range)
+    out_fname = graphdata_fname.replace('.txt', '.csv')
+    if part_range:
+        out_fname = f"part_({res_df.index[0]}-{res_df.index[-1]})" + out_fname
+    save_results(res_df, out_fname)
 
 
 if __name__ == "__main__":
     fname = GDataFname.new(8)
-    solve_whole_dataset(fname.adjlist_fname, p_max=3)
+    solve_dataset(fname.adjlist_fname, p_max=3, part_range=(514, 11117))
